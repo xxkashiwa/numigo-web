db2182393c650b19a9b2e1451a41ad9f819bd631
xxcypress <xxcypres@gmail.com>
2025-03-09 18:18:21 +0800

    fix: vercel Build Failed

--- src/components/bubbles/ai-bubble.tsx
+++ src/components/bubbles/ai-bubble.tsx
@@ -1,10 +1,7 @@
-import MarkdownRenderer from '../markdown-render';
 const AiBubble = ({ message }: { message: string }) => {
   return (
     <div className="flex w-full">
-      <div className="w-full">
-        <MarkdownRenderer markdown={message} />
-      </div>
+      <div className="w-full">{message}</div>
     </div>
   );
 };
--- src/components/bubbles/user-bubble.tsx
+++ src/components/bubbles/user-bubble.tsx
@@ -1,10 +1,7 @@
-import MarkdownRenderer from '../markdown-render';
 const UserBubble = ({ message }: { message: string }) => {
   return (
     <div className="flex w-full justify-end">
-      <div className="rounded-3xl bg-gray-200 p-2">
-        <MarkdownRenderer markdown={message} />
-      </div>
+      <div className="rounded-3xl bg-gray-200 p-2">{message}</div>
     </div>
   );
 };
