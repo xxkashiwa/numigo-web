9a88b3f8c49c99ab390022cb24bb34c5555a0170
xxcypress <xxcypres@gmail.com>
2025-03-25 00:07:28 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -4,10 +4,10 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://119.45.26.22:8123/api/:path*', // 替换为你的实际 API 地址
+        destination: 'http://119.45.26.22:8123/:path*', // 替换为你的实际 API 地址
       },
     ];
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
