b2605694f06dab7004ed5755a9d6bc8a97bb0521
kashiwa <xxcypres@gmail.com>
2025-03-27 21:06:21 +0800

    fix: logic of home page navigate to chat page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 'use client';
-
+import { ChatMessageList } from '@/components/chat-message-list';
 import InputBox from '@/components/inputbox';
 import { useConversation } from '@/hooks/use-conversation';
 const Home = () => {
   const { isLoading, chatLogs } = useConversation();
-  if (chatLogs.length > 0) {
+  if (chatLogs.length > 0 && !isLoading) {
     window.location.href = '/chat';
   }
   return (
@@ -12,7 +12,11 @@ const Home = () => {
       <div className="flex h-full w-full items-center justify-center">
         <div className="flex h-full w-full flex-col items-center justify-center gap-7 md:max-w-3xl">
           {isLoading ? (
-            ''
+            <div className="flex w-full flex-col items-center">
+              <div className="w-full md:max-w-3xl">
+                <ChatMessageList />
+              </div>
+            </div>
           ) : (
             <h1 className="text-3xl font-extrabold">有什么可以帮忙的？</h1>
           )}
